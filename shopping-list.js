777a32c7f04cfb1ce9bd8865500fbfc6b0ba91af
Mijuoska <miika.kallasoja@gmail.com>
2020-01-18 20:12:01 +0200

    Update shopping-list.js

--- shopping-list.js
+++ shopping-list.js
@@ -1,72 +1,115 @@
-let shoppingList = []
+const shoppingList = []
 
-// sort products by price
 
-const sortItemsByPrice = function (list) {
-    list.sort(function (a, b) {
+
+const sortItemsByPrice = function (shoppingList, sortOrderA = -1, sortOrderB = 1) {
+    shoppingList.sort(function (a, b) {
         if (a.price > b.price) {
-            return -1
+            return sortOrderA
         } else if (b.price > a.price) {
-            return 1
+            return sortOrderB
         } else {
             return 0
         }
-    })
-}
-
-const renderList = function () {
-    sortItemsByPrice(shoppingList)
-    shoppingList.forEach(function (item) {
-        listItem = document.createElement("li")
-        listItem.textContent = `${item.item} ${item.price} €`
-        listItem.id = `${item.item}`
-        document.querySelector("#shopping-list").appendChild(listItem)
-    })
-
-};
-
-shoppingList = JSON.parse(localStorage.getItem('shoppingList'));
-sortItemsByPrice(shoppingList)
-renderList()
-
-// add a product
-const addItem = function (item, price) {
-    shoppingList.push({item: item, price: price})
-    localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-}
-
-document.querySelector("#item-name").addEventListener("input", function (e) {
-    itemInput = e.target.value
-});
- 
-document.querySelector("#item-price").addEventListener("input", function (e) {
-    priceInput = Number(e.target.value)
-});
-
-
-document.querySelector("#add-item").addEventListener("click", function () {
-    addItem(itemInput, priceInput)
-});
-
-shoppingList.forEach(function (item) {
-    document.querySelector(`#${item.item}`).addEventListener("click", function (){
-        deleteItem(item.item)
     });
-});
+}
+
+
+// sum up total price
+
+const sumUpTotal = function () {
+    let total = 0
+    shoppingList.forEach(function (item) {
+        total += item.price
+    })
+    return total
+}
+
+// render total
+
+const renderTotal = function () {
+    let total = document.querySelector("#total-price")
+    total.innerHTML = `${sumUpTotal()} €`
+}
+
 
 
 
 // delete an item
 const deleteItem = function (itemName) {
     const index = shoppingList.findIndex(function (item) {
-        return item.item.toLowerCase() === itemName.toLowerCase()
+        return item.name.toLowerCase() === itemName.toLowerCase()
     })
     if (index > -1) {
         shoppingList.splice(index, 1)
         document.querySelector(`#${itemName}`).remove()
-    } 
-    localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+        // We also need to calculate the total again and render it
+        sumUpTotal()
+        renderTotal()
+    }
 }
+
+    
+
+
+const renderList = function (sortOrderA, sortOrderB) {
+    document.querySelector("#shopping-list").innerHTML = ""
+    sortItemsByPrice(shoppingList, sortOrderA, sortOrderB)
+    shoppingList.forEach(function (item) {
+        listItem = document.createElement("li")
+        listItem.id = `${item.name}`
+        listItem.innerHTML = `<span class="item">${item.name}</span> <span class="price">${item.price} €</span></span>`
+        document.querySelector("#shopping-list").appendChild(listItem)
+        // we need to add "deletability to each item as they are createdS"
+        document.getElementById(`${item.name}`).addEventListener("click", function () {
+                deleteItem(item.name)
+            });
+        sumUpTotal()
+        renderTotal()
+    });
+
+};
+
+
+
+
+document.querySelector("#add-item-form").addEventListener("submit", function (e) {
+    e.preventDefault()
+    name = e.target.elements.itemName.value 
+    price = Number(e.target.elements.itemPrice.value)
+    if (name !== "" && price !== "") {
+        shoppingList.push({ name: name, price: price })
+        e.target.elements.itemName.value = ""
+        e.target.elements.itemPrice.value = ""
+    } else {
+        alert("Please provide a value to both the name and price fields")
+    }
+    renderList()
+});
+
+
+
+sortOptions = document.getElementsByName("sort")
+sortOptions.forEach(function (option) {
+    option.addEventListener("change", function (e) {
+    let sortOrder = e.target.value
+    if (sortOrder === "desc") {
+        sortOrderA = -1
+        sortOrderB = 1
+    } else if (sortOrder === "asc") {
+        sortOrderA = 1
+        sortOrderB = -1
+    }
+    renderList(sortOrderA, sortOrderB)
+})
+
+})
+
+
+
+
+
+
 
 
 
